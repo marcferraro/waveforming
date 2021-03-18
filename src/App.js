4f90478552254428ebfd6e989fcfe2cee68a4d2d
Marc <marc.ferraro1@gmail.com>
2021-03-18 13:01:12 -0500

    Canvases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 import OverlappingWaveformInterface from './components/OverlappingWaveformInterface'
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route path='/new-overlapping-waveform' component={OverlappingWaveformInterface}/>
       </Switch>
--- src/components/OverlappingWaveformInterface.js
+++ src/components/OverlappingWaveformInterface.js
@@ -1,6 +1,35 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import overlapInterface from '../overlapInterface.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+}));
 
 export default function OverlappingWaveformInterface(){
+    const classes = useStyles();
+    
     return(
-        <div>hello from interface</div>
+        <div>
+            <Grid container className={classes.root} spacing={2}>
+                    <Grid container justify="center" spacing={9}>
+                        <Grid item>
+                            <canvas id="input" width="48" height="48" style={{width:"240px", height:"240px"}}></canvas>
+                        </Grid>
+                        <Grid item>
+                            <canvas id="output" width="48" height="48" style={{width:"240px", height:"240px"}}></canvas>
+                        </Grid>
+                    </Grid>
+            </Grid>
+        </div>
     )
 }
