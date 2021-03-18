bb1e44776b5039d8088d6d9eae0e82567bf94151
Marc <marc.ferraro1@gmail.com>
2021-03-18 15:55:10 -0500

    Early Slider

--- src/components/OverlappingWaveformInterface.js
+++ src/components/OverlappingWaveformInterface.js
@@ -1,11 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import '../overlapInterface.css'
-import Paper from '@material-ui/core/Paper';
+import { Grid, Paper, Switch, Select, MenuItem, FormControl, FormGroup, FormControlLabel, FormHelperText, InputLabel, Slider } from '@material-ui/core';
+// import Paper from '@material-ui/core/Paper';
 // import Checkbox from '@material-ui/core/Checkbox';
-import Switch from '@material-ui/core/Switch';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Switch from '@material-ui/core/Switch';
+// import FormGroup from '@material-ui/core/FormGroup';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Select from '@material-ui/core/Select';
+// import FormHelperText from '@material-ui/core/FormHelperText';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import FormControl from '@material-ui/core/FormControl';
+
+import '../overlapInterface.css'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -35,7 +41,46 @@ export default function OverlappingWaveformInterface(){
                 <Grid item xs={12}>
                     <Paper>
                         <Grid container direction="row" justify="space-evenly" alignItems="flex-start" spacing={1}>
-                            <FormGroup >
+                            {/* <FormGroup row> */}
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel id="n-input">N</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={2}
+                                        onChange={() => console.log("select!")}
+                                    >
+                                        <MenuItem value={2}>2</MenuItem>
+                                        <MenuItem value={3}>3</MenuItem>
+                                        <MenuItem value={4}>4</MenuItem>
+                                    </Select>
+                                    <FormHelperText>?</FormHelperText>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel id="symmetry-input">Symmetry</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={1}
+                                        onChange={() => console.log("select!")}
+                                    >
+                                        <MenuItem value={1}>1</MenuItem>
+                                        <MenuItem value={2}>2</MenuItem>
+                                        <MenuItem value={3}>3</MenuItem>
+                                        <MenuItem value={4}>4</MenuItem>
+                                        <MenuItem value={5}>5</MenuItem>
+                                        <MenuItem value={6}>6</MenuItem>
+                                        <MenuItem value={7}>7</MenuItem>
+                                        <MenuItem value={8}>8</MenuItem>
+                                    </Select>
+                                    <FormHelperText>?</FormHelperText>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel id="ground-input">Ground</InputLabel>
+                                    <Grid item xs>
+                                    </Grid>
+                                    <FormHelperText>?</FormHelperText>
+                                </FormControl>
                                 <FormControlLabel
                                     control={<Switch 
                                         // checked={state.checkedB}
@@ -46,17 +91,18 @@ export default function OverlappingWaveformInterface(){
                                 />
                                 <FormControlLabel
                                     control={<Switch 
-                                    // checked={state.checkedB}
+                                        // checked={state.checkedB}
                                     onChange={() => console.log("switch!")}
                                     name="periodic switch"
                                     color="primary"/>}
                                     label="Periodic Output"
                                 />
-                            </FormGroup>
+                            {/* </FormGroup> */}
                         </Grid>
                     </Paper>
                 </Grid>
             </Grid>
+                                    <Slider defaultValue={30} onChange={() => console.log("slider!")} aria-labelledby="continuous-slider" />
         </div>
     )
 }
