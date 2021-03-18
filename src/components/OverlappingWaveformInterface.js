f5d3d6721e2d2ff5284618e07aa5be03e98a777d
Marc <marc.ferraro1@gmail.com>
2021-03-18 13:12:22 -0500

    basic interface styling

--- src/components/OverlappingWaveformInterface.js
+++ src/components/OverlappingWaveformInterface.js
@@ -1,14 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import overlapInterface from '../overlapInterface.css'
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
-    },
-    paper: {
-      height: 140,
-      width: 100,
     },
     control: {
       padding: theme.spacing(2),
@@ -21,7 +18,8 @@ export default function OverlappingWaveformInterface(){
     return(
         <div>
             <Grid container className={classes.root} spacing={2}>
-                    <Grid container justify="center" spacing={9}>
+                <Grid item xs={12}>
+                    <Grid container direction="row" justify="space-evenly" alignItems="flex-start" spacing={1} >
                         <Grid item>
                             <canvas id="input" width="48" height="48" style={{width:"240px", height:"240px"}}></canvas>
                         </Grid>
@@ -29,6 +27,14 @@ export default function OverlappingWaveformInterface(){
                             <canvas id="output" width="48" height="48" style={{width:"240px", height:"240px"}}></canvas>
                         </Grid>
                     </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper>
+                        <Grid container direction="row" justify="space-evenly" alignItems="flex-start" spacing={1}>
+                            buttons and attributes n stuff
+                        </Grid>
+                    </Paper>
+                </Grid>
             </Grid>
         </div>
     )
