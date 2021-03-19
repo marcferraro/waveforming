aab9a3b9faac0696b0c3bc300c9cc4a06aa4d9b3
Marc <marc.ferraro1@gmail.com>
2021-03-19 15:24:25 -0500

    List buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,13 @@
 import Drawer from '@material-ui/core/Drawer';
 import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const drawerWidth = 200;
 
@@ -26,11 +34,36 @@ export default function Navbar(){
     const classes = useStyles();
 
     return(
-        <div>
+        <div className={classes.root}>
+            <CssBaseline />
             <Drawer
+                className={classes.drawer}
+                classes={{
+                    paper: classes.drawerPaper,
+                  }}
                 variant="permanent"
                 anchor="left"
-            >hello</Drawer>
+            >
+            <Divider />
+            <div className={classes.toolbar} />
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+                </ListItem>
+            ))}
+            </List>
+            <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+            </Drawer>
         </div>
     )
 }
