0615555cb8f75b3c9c2159d55650146822dc5c6a
Marc <marc.ferraro1@gmail.com>
2021-03-19 15:11:52 -0500

    Basic Drawer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,36 @@
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 200;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    }
+  }));
+
+
 
 export default function Navbar(){
+    const classes = useStyles();
+
     return(
-        <div>hello from Navbar</div>
+        <div>
+            <Drawer
+                variant="permanent"
+                anchor="left"
+            >hello</Drawer>
+        </div>
     )
 }
