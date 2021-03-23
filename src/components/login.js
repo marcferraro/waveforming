e1f956de59f3db0e91dc261805f4755680f0a526
Marc <marc.ferraro1@gmail.com>
2021-03-23 10:27:10 -0500

    login handlers

--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { Grid } from '@material-ui/core'
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -15,7 +16,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Login(props){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
     const classes = useStyles()
+
+    const handleUsername = event => {
+        setUsername(event.target.value)
+    }
+
+    const handlePassword = event => {
+        setPassword(event.target.value)
+    }
 
     const handleSubmit = () => {
         console.log('submit')
@@ -25,7 +37,8 @@ function Login(props){
         <div>
             <Grid container justify="space-evenly" >
                 <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
-                        <TextField onChange={(event) => console.log('change')} id="outlined-basic" label="Username" variant="outlined" />
+                        <TextField onChange={handleUsername} value={username} id="username-field" label="Username" variant="outlined" />
+                        <TextField onChange={handlePassword} value={password} type="password" id="password-field" label="password" variant="outlined" />
                         <Button type="submit" variant="contained" color="secondary"endIcon={<ArrowForwardIosIcon />}>Submit</Button>
                 </form>
             </Grid>
