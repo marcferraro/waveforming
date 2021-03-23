36d5b43a2bfb7d5047c45aa474f9370e457cb78a
Marc <marc.ferraro1@gmail.com>
2021-03-23 12:38:57 -0500

    Dashboard skeleton and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import OverlappingWaveformInterface from './components/OverlappingWaveformInterface'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
+import Dashboard from './components/Dashboard';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path='/new-overlapping-waveform' component={OverlappingWaveformInterface}/>
         <Route path='/login' component={Login}/>
+        <Route path='/dashboard' component={Dashboard}/>
       </Switch>
     </div>
   );
---
+++ src/components/Dashboard.js
@@ -0,0 +1,7 @@
+function Dashboard(){
+    return(
+        <div>hello from the dashboard</div>
+    )
+}
+
+export default Dashboard
