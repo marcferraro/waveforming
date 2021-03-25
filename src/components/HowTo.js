1ff3fd185ca33fc324b579edb4b2ccef48a7174a
Marc <marc.ferraro1@gmail.com>
2021-03-25 00:18:18 -0500

    Basic HowTo

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -1,8 +1,24 @@
+import Typography from '@material-ui/core/Typography';
+
 const HowTo = props => {
 
     return(
         <div style={{marginLeft: 180, marginRight: 'auto'}} >
-            Hello from the HowTo
+            <Typography paragraph>
+                What is the WaveFunctionCollapse link to project repo and how are we using it? Created by Maxim Gumin link to github
+                the function takes in an initial pattern bitmap and creates an output bitmap based on that pattern. This app will riff
+                on pixel art created by you.
+
+                You can select colors to draw on the canvas with. There are some additional settings too that will have an impact on the
+                output. An explanation for each:
+            </Typography>
+            <Typography paragraph>
+                N: The N value can be set between 2-4(WAIT APPARANTLY IT CAN GO HIGHER??) and controls the size of the patterns the program will look for. Lower values can result
+                in an image being deconstructed into many small pieces, while higher values create more uniform outputs.
+            </Typography>
+            <Typography paragraph>
+                Symmetry: The Symmetry value can be set between 1-8 and controls to what degree the program will flip and twist the input pattern.
+            </Typography>
         </div>
     )
 }
