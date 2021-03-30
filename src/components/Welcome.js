9b7d7adf33b87d284914f75f18cd1bbb443b6941
Marc <marc.ferraro1@gmail.com>
2021-03-29 21:58:13 -0500

    background video welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { Link }  from 'react-router-dom'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import ReactPlayer from 'react-player'
+import classes from './BackgroundVideo.module.css';
 
 const Welcome = props => {
 
@@ -11,21 +13,38 @@ const Welcome = props => {
     //     if (token){
     //       props.history.push('/dashboard')
     //     }}, [])
-
+    const videoSource = "https://waveforming.s3.us-east-2.amazonaws.com/WaveFunctionCollapse.mp4"
 
     return(
-        <div style={{marginLeft: 180, marginRight: 'auto', marginTop: 80}} >
-                <Typography >
-                    waveforming
-                </Typography>
-                <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Button>Login</Button>
-                </Link>
-                <Link to='/sign-up' style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Button>Sign-Up</Button>
-                </Link>
+        <div className={classes.container} >
+
+                {/* <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
+                    <source src={videoSource} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video> */}
+
+            <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
+                <source src={videoSource} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+                <div className={classes.Content}>
+                    <div className={classes.SubContent} >
+
+                    <Typography >
+                        waveforming
+                    </Typography>
+                    <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Button>Login</Button>
+                    </Link>
+                    <Link to='/sign-up' style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Button>Sign-Up</Button>
+                    </Link>
+                    </div>
+                </div>
         </div>
     )
 }
+// style={{marginLeft: 180, marginRight: 'auto', marginTop: 80}}
 
 export default Welcome
