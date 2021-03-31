09cafca3ecb88bd0a23a2091b63531ce525e8a98
Marc <marc.ferraro1@gmail.com>
2021-03-31 15:20:08 -0500

    oOutput card canvas

--- src/components/OOutputCard.js
+++ src/components/OOutputCard.js
@@ -6,29 +6,48 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { useEffect, useRef } from 'react';
+import './OOutputCard.css'
 
 const useStyles = makeStyles({
     root: {
       maxWidth: 345,
     },
     media: {
-      height: 80,
-      width: 80
+      height: 48,
+      width: 48,
+      resize: {
+          width: 220,
+          height: 220
+      }
     },
 });
 
 const OOutputCard = props => {
     const classes = useStyles()
+    const canvasRef = useRef(null)
     // debugger
+
+    useEffect(() => {
+        const ctx = canvasRef.current.getContext('2d')
+        const image = document.createElement('img')
+        image.src = `http://localhost:3000${props.oOutput.ooutput.url}`
+        image.onload = () => {
+            ctx.drawImage(image,0,0)
+        }
+    }, [])
+    
     return(
         <Grid item>
             <Card className={classes.root}>
                 <CardActionArea>
-                    <CardMedia
+                    {/* <CardMedia
                         className={classes.media}
                         image={`http://localhost:3000${props.oOutput.ooutput.url}`}
                         title="Overlapping Wave Function Output"
-                    />
+                    /> */}
+                    {/* <img src={`http://localhost:3000${props.oOutput.ooutput.url}`} width="48" height="48" style={{width:"240px", height:"240px"}}/> */}
+                    <canvas width="48" height="48" style={{width:"220px", height:"220px"}} ref={canvasRef}/>
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
                         {props.oOutput.title ? props.oOutput.title : "Untitled"}
