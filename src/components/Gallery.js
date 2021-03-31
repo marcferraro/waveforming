4991e1fb2c31c7efbb5aa6cce4fd854bb4a32ee5
Marc <marc.ferraro1@gmail.com>
2021-03-31 13:25:34 -0500

    basic cards appearing

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,9 @@
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { useSelector } from 'react-redux'
+import OOutputCard from './OOutputCard'
 
 const Gallery = props => {
 
@@ -19,7 +15,7 @@ const Gallery = props => {
                 Gallery
             </Typography>
             <Grid container>
-                {oOutputs.map(o => <p key={o.id}>o.title</p>)}
+                {oOutputs.map(o => <OOutputCard key={o.id} oOutput={o}/>)}
             </Grid>
         </div>
     )
--- src/components/OOutputCard.js
+++ src/components/OOutputCard.js
@@ -4,6 +4,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -16,36 +17,38 @@ const useStyles = makeStyles({
 });
 
 const OOutputCard = props => {
-
+    const classes = useStyles()
 
 
     return(
-        <Card className={classes.root}>
-            <CardActionArea>
-                <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Overlapping Wave Function Output"
-                />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                Share
-                </Button>
-                <Button size="small" color="primary">
-                Learn More
-                </Button>
-            </CardActions>
-        </Card>
+        <Grid item>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                    className={classes.media}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Overlapping Wave Function Output"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                    Share
+                    </Button>
+                    <Button size="small" color="primary">
+                    Learn More
+                    </Button>
+                </CardActions>
+            </Card>
+        </Grid>
     )
 }
 
