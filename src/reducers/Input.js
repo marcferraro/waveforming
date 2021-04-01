b0fd7de6b01ac17c767e7b26b9c28f86ff798b4a
Marc <marc.ferraro1@gmail.com>
2021-03-31 22:56:36 -0500

    input creation case

--- src/reducers/Input.js
+++ src/reducers/Input.js
@@ -3,10 +3,11 @@ const inputReducer = (state = [], action) => {
     switch(action.type){
         case 'LOGIN_SUCCESS':
           return []
+        case 'INPUT_CREATION_SUCCESS':
+            return [...state, action.input]
         default: 
             return state
     }
 }
     
-    export default authReducer
-    //   case 'CURRENT_USER':
+    export default inputReducer
