824c3fe1bdc37f8718b3ffe870f6c18dac76dc54
Marc <marc.ferraro1@gmail.com>
2021-04-01 02:06:13 -0500

    basic show page wired

--- src/components/OOutputShow.js
+++ src/components/OOutputShow.js
@@ -1,73 +1,81 @@
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import Typography from '@material-ui/core/Typography';
-// import { Button, Grid } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
-// import { useEffect, useRef } from 'react';
-// import IconButton from '@material-ui/core/IconButton';
-// import StarsIcon from '@material-ui/icons/Stars';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Button, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useEffect, useRef, useState } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import StarsIcon from '@material-ui/icons/Stars';
+import { useSelector, useDispatch } from 'react-redux'
 
-// const useStyles = makeStyles({
-//     root: {
-//       maxWidth: 345,
-//     },
-//     media: {
-//       height: 48,
-//       width: 48,
-//       resize: {
-//           width: 220,
-//           height: 220
-//       }
-//     },
-// });
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 48,
+      width: 48,
+      resize: {
+          width: 220,
+          height: 220
+      }
+    },
+});
 
-// const OOutputCard = props => {
-//     const classes = useStyles()
-//     const canvasRef = useRef(null)
-//     // debugger
+const OOutputShow = props => {
+    const classes = useStyles()
+    const canvasRef = useRef(null)
+    const oOutputs = useSelector(state => state.oOutputs)
+    const [oOutput, setOOutput] = useState(null)
 
-//     useEffect(() => {
-//         const ctx = canvasRef.current.getContext('2d')
-//         const image = document.createElement('img')
-//         image.src = `http://localhost:3000${props.oOutput.ooutput.url}`
-//         image.onload = () => {
-//             ctx.drawImage(image,0,0)
-//         }
-//     }, [])
+    useEffect(() => {
+        const findResult = oOutputs.find(o => o.id === parseInt(props.match.params.id, 10))
+        setOOutput(findResult)
+        prepCanvas()
+    }, [])
+
+    const prepCanvas = () => {
+        const ctx = canvasRef.current.getContext('2d')
+        const image = document.createElement('img')
+        image.src = `http://localhost:3000${oOutput.ooutput.url}`
+        image.onload = () => {
+            ctx.drawImage(image,0,0)
+        }
+    }
     
-//     return(
-//         <Grid item>
-//             <Card className={classes.root}>
-//                 <CardActionArea>
-//                     <canvas width="48" height="48" style={{width:"220px", height:"220px", border: '0px none black'}} ref={canvasRef}/>
-//                     <CardContent>
-//                     <Typography gutterBottom variant="h5" component="h2">
-//                         {props.oOutput.title ? props.oOutput.title : "Untitled"}
-//                     </Typography>
-//                     <Typography variant="body2" color="textSecondary" component="p">
-//                         Artist: {props.oOutput.user ? props.oOutput.user.username : "unknown"}
-//                     </Typography>
-//                     </CardContent>
-//                 </CardActionArea>
-//                 <CardActions>
-//                     <Grid container justify="flex-start">
-//                         <Grid item xs={10}>
-//                             <Button size="small" color="primary">
-//                             View Input
-//                             </Button>
-//                         </Grid>
-//                         <Grid item >
-//                             <IconButton size="small" color="primary">
-//                                 <StarsIcon />
-//                             </IconButton>
-//                         </Grid>
-//                     </Grid>
-//                 </CardActions>
-//             </Card>
-//         </Grid>
-//     )
-// }
+    return(
+        <Grid item>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <canvas width="48" height="48" style={{width:"220px", height:"220px", border: '0px none black'}} ref={canvasRef}/>
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {oOutput ? oOutput.title : "Untitled"}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Artist: {oOutput ? oOutput.user.username : "unknown"}
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Grid container justify="flex-start">
+                        <Grid item xs={10}>
+                            <Button size="small" color="primary">
+                            View Input
+                            </Button>
+                        </Grid>
+                        <Grid item >
+                            <IconButton size="small" color="primary">
+                                <StarsIcon />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </CardActions>
+            </Card>
+        </Grid>
+    )
+}
 
-// export default OOutputCard
+export default OOutputShow
