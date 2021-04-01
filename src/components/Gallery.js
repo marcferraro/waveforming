0530e773da393f342b6f2caf244df7e980365b67
Marc <marc.ferraro1@gmail.com>
2021-04-01 11:31:50 -0500

    commented unused imports

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,7 @@
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
+// import { makeStyles } from '@material-ui/core/styles';
+// import clsx from 'clsx';
 import { useSelector } from 'react-redux'
 import OOutputCard from './OOutputCard'
 
