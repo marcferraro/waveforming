b1e8fda463c28d4b992e17c9c8af783249d88aef
Marc <marc.ferraro1@gmail.com>
2021-03-31 22:51:20 -0500

    reducer action

--- src/reducers/Ooutput.js
+++ src/reducers/Ooutput.js
@@ -5,6 +5,8 @@ const oOutputReducer = (state = [], action) => {
           return action.oOutputs
         case 'UPDATE_OOUTPUT':
             return state.map(o => o.id === action.oOutput.id ? action.oOutput : o)
+        case 'OOUTPUT_CREATION_SUCCESS':
+            return [...state, action.oOutput]
         case 'LOGOUT':
             return []
         default: 
