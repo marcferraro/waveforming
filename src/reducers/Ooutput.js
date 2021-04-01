2c8af19bd57c8c46663fcc1c215812ff3a7d669b
Marc <marc.ferraro1@gmail.com>
2021-03-31 22:09:34 -0500

    update_ooutput case

--- src/reducers/Ooutput.js
+++ src/reducers/Ooutput.js
@@ -2,9 +2,9 @@ const oOutputReducer = (state = [], action) => {
     
     switch(action.type){
         case 'FETCH_OOUTPUTS_SUCCESS':
-            // console.log('wired')
-            // debugger
           return action.oOutputs
+        case 'UPDATE_OOUTPUT':
+            return state.map(o => o.id === action.data.id ? action.data : o)
         case 'LOGOUT':
             return []
         default: 
