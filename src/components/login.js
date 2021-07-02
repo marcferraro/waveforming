d4c7ffc09faa09f9102599c29dce3b7d8b065629
Marc <marc.ferraro1@gmail.com>
2021-07-02 12:42:09 -0500

    arrow icon cut

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { Grid, Typography } from '@material-ui/core'
 import { useState } from 'react';
 import { useDispatch } from "react-redux";
